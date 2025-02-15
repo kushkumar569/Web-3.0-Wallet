b6d5c56b8ba0b307e2c4c51d2d281d553f8a4b42
kushkumar569 <kushk569@gmail.com>
2025-02-15 19:07:47 +0530

    Updated project

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import NodePolyfills from 'vite-plugin-node-polyfills';
+import {NodePolyfills }from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
-    react()
-    // NodePolyfills()
+    react(),
+    NodePolyfills()
   ],
   base: '/Web-3.0-Wallet/',
 });
